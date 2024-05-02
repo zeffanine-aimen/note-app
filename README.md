679219834b8adc619042ff41955346a054411c8f
Zeffanine Aimen <zeffanine16@gmail.com>
2024-05-02 15:06:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div dir="rtl">
+<h1> مسار أساسيات React.js </h1>
+<p>الشيفرة المصدرية الخاصة بمسار أساسيات React.js ضمن دورة "تطوير التطبيقات باستخدام JavaScript" المقدمة من أكاديمية حسوب</p>
+
+<div>
+<a href="https://academy.hsoub.com/learn/javascript-application-development/">دورة تطوير التطبيقات باستخدام JavaScript</a>
+</div>
+<h2> المتطلبات </h2>
+<ul>
+  <li>NodeJS</li>
+  <li>MongoDb</li>
+  <li>Git</li>
+  <li>Nodemon</li>
+</ul>
+<h2> طريقة الإعداد </h2>
+<ul>
+  <li>نسخ المستودع <code>git clone https://github.com/HsoubAcademy/reactjs-basics.git</code></li>
+  <li>الانتقال إلى المجلد <code>cd reactjs-basics</code></li>
+  <li>تشغيل <code>npm install</code></li>
+  <li>تشغيل <code>npm run start</code> لتشغيل المشروع</li>
+</ul>
+</div>
---
+++ src/App.js
@@ -0,0 +1,230 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import NotesContainer from './components/Notes/NotesContainer';
+import NotesList from './components/Notes/NotesList';
+import Note from './components/Notes/Note';
+import NoteForm from './components/Notes/NoteForm';
+import Preview from './components/Preview';
+import Message from './components/Message';
+import Alert from './components/Alert';
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [creating, setCreating] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [validationErrors, setValidationErrors] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('notes')) {
+      setNotes(JSON.parse(localStorage.getItem('notes')));
+    } else {
+      localStorage.setItem('notes', JSON.stringify([]));
+      setNotes([]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (validationErrors.length !== 0) {
+      setTimeout(() => {
+        setValidationErrors([]);
+      }, 3000);
+    }
+  }, [validationErrors]);
+
+  // حفظ في الذاكرة المحلية
+  const saveToLocalStorage = (name, item) => {
+    localStorage.setItem(name, JSON.stringify(item));
+  };
+
+  // التحقق من البيانات
+  const validate = () => {
+    const validationErrors = [];
+    let passed = true;
+    if (!title) {
+      validationErrors.push('الرجاء إدخال عنوان الملاحظة');
+      passed = false;
+    }
+    if (!content) {
+      validationErrors.push('الرجاء إدخال محتوى الملاحظة');
+      passed = false;
+    }
+    setValidationErrors(validationErrors);
+    return passed;
+  };
+
+  //تغيير عنوان الملاحظة
+  const changeTitleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  //تغيير نص الملاحظة
+  const changeContentHandler = (event) => {
+    setContent(event.target.value);
+  };
+
+  //الانتقال إلى وضع إضافة ملاحظة
+  const addNoteHandler = () => {
+    setCreating(true);
+    setTitle('');
+    setContent('');
+    setEditing(false);
+  };
+
+  //اختيار ملاحظة
+  const selectNoteHandler = (noteId) => {
+    setSelectedNote(noteId);
+    setCreating(false);
+    setEditing(false);
+  };
+
+  //حفظ الملاحظة
+  const saveNoteHandler = () => {
+    if (!validate()) return;
+    const note = {
+      id: new Date(),
+      title: title,
+      content: content,
+    };
+    const updatedNotes = [...notes, note];
+    saveToLocalStorage('notes', updatedNotes);
+    setNotes(updatedNotes);
+    setTitle('');
+    setContent('');
+    setCreating(false);
+    setSelectedNote(note.id);
+  };
+
+  //حذف الملاحظة
+  const deleteNoteHandler = (noteId) => {
+    const updatedNotes = [...notes];
+    const noteIndex = updatedNotes.findIndex((note) => note.id === noteId);
+    updatedNotes.splice(noteIndex, 1);
+    saveToLocalStorage('notes', updatedNotes);
+    setNotes(updatedNotes);
+    setSelectedNote(null);
+  };
+
+  //الانتقال إلى وضع تعديل الملاحظة
+  const editNoteHandler = (noteId) => {
+    const note = notes.find((note) => note.id === noteId);
+
+    setEditing(true);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+  // حفظ تعديلات الملاحظة
+  const updateNoteHandler = () => {
+    if (!validate()) return;
+    const updatedNotes = [...notes];
+    const noteIndex = notes.findIndex((note) => note.id === selectedNote);
+    updatedNotes[noteIndex] = {
+      id: selectedNote,
+      title: title,
+      content: content,
+    };
+    saveToLocalStorage('notes', updatedNotes);
+
+    setNotes(updatedNotes);
+    setEditing(false);
+    setTitle('');
+    setContent('');
+  };
+
+  //إحضار نموذج إضافة ملاحظة
+  const getAddNote = () => {
+    return (
+      <NoteForm
+        formTitle="ملاحظة جديدة"
+        title={title}
+        content={content}
+        titleChanged={changeTitleHandler}
+        contentChanged={changeContentHandler}
+        submitText="حفظ"
+        submitClicked={saveNoteHandler}
+      />
+    );
+  };
+
+  //إحضار قسم عرض الملاحظة
+  const getPreview = () => {
+    if (notes.length === 0) {
+      return <Message title="لا يوجد ملاحظات" />;
+    }
+
+    if (!selectedNote) {
+      return <Message title="الرجاء اختيار ملاحظة" />;
+    }
+
+    const note = notes.find((note) => {
+      return note.id === selectedNote;
+    });
+
+    let noteDisplay = (
+      <div>
+        <h2>{note.title}</h2>
+        <p>{note.content}</p>
+      </div>
+    );
+
+    if (editing) {
+      noteDisplay = (
+        <NoteForm
+          formTitle="تعديل ملاحظة"
+          title={title}
+          content={content}
+          titleChanged={changeTitleHandler}
+          contentChanged={changeContentHandler}
+          submitText="تعديل"
+          submitClicked={updateNoteHandler}
+        />
+      );
+    }
+
+    return (
+      <div>
+        {!editing && (
+          <div className="note-operations">
+            <a href="#" onClick={() => editNoteHandler(note.id)}>
+              <i className="fa fa-pencil-alt" />
+            </a>
+            <a href="#" onClick={() => deleteNoteHandler(note.id)}>
+              <i className="fa fa-trash" />
+            </a>
+          </div>
+        )}
+        {noteDisplay}
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      <NotesContainer>
+        <NotesList>
+          {notes.map((note) => (
+            <Note
+              key={note.id}
+              title={note.title}
+              active={selectedNote === note.id}
+              noteClicked={() => selectNoteHandler(note.id)}
+            />
+          ))}
+        </NotesList>
+        <button className="add-btn" onClick={addNoteHandler}>
+          +
+        </button>
+      </NotesContainer>
+      <Preview>{creating ? getAddNote() : getPreview()}</Preview>
+
+      {validationErrors.length !== 0 && (
+        <Alert validationMessages={validationErrors} />
+      )}
+    </div>
+  );
+}
+
+export default App;
